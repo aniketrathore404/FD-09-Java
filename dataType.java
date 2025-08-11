113eab3602ef3d5e3712ca5b357020943c03116f
Aniket Rathore 0217 <rathoreaniket404@gmail.com>
2025-08-11 11:24:24 +0530

    Add files via upload

---
+++ dataType.java
@@ -0,0 +1,22 @@
+public class dataType
+{
+    public static void main(String[] args) {
+
+        int a = 5;
+        float b = 0.5f;
+        double c = 3.14;
+        byte d = 100;
+        short e = 32000;
+        char f = 'a';
+        boolean g = true;
+        long h = 1234567890L;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
