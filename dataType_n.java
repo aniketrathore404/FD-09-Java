c3971d8642a928527af5c9dc6e9d10c687628cc4
Aniket Rathore 0217 <rathoreaniket404@gmail.com>
2025-08-12 08:41:51 +0530

    Add files via upload

---
+++ dataType_n.java
@@ -0,0 +1,13 @@
+public class dataType_n
+{
+    public static void main(String[] args) {
+        String name = "Aniket";
+        System.out.println(name);
+
+        int b[] = {0,2,6};
+        System.out.println(b[0]);
+        System.out.println(b[1]);
+        System.out.println(b[2]);
+        }
+
+    }
