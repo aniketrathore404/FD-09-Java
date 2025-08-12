8adcdddc282d25dd5a8ee100d6e642ebb5e7f2fd
Aniket Rathore 0217 <rathoreaniket404@gmail.com>
2025-08-12 09:18:47 +0530

    Add files via upload

---
+++ Userinput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Userinput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("(Enter a no)");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+    }
+}
