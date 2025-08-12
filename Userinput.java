6ba1e6e98c25bb1e334bb78bdfdca8bca191777c
Aniket Rathore 0217 <rathoreaniket404@gmail.com>
2025-08-12 09:41:08 +0530

    Update Userinput.java

--- Userinput.java
+++ Userinput.java
@@ -1,10 +1,21 @@
+//import java.util.Scanner;
+//public class Userinput {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("(Enter a no)");
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        System.out.println(a+b);
+//    }
+//}
+
 import java.util.Scanner;
 public class Userinput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("(Enter a no)");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(a+b);
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        System.out.println(a + b);
     }
 }
